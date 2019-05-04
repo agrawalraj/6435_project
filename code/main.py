9bcc02a4e36c3e135d773459bee486e2109ccd3a
Raj <raj@Rajs-MacBook-Pro-3.local>
2019-05-03 21:09:21 -0400

    fixed bug

--- code/main.py
+++ code/main.py
@@ -20,19 +20,19 @@ if __name__ == "__main__":
     for snr in snr_arr:
         for p in p_arr:
             print('Simulating for N = {0} and p = {1}'.format(N, p))
-            X, y = make_simple_interaction_data(N, p, m0, snr_stength)
-            np.save('../data/synthetic/X_N_{0}_M_{1}_scale_{2}'.format(N, M, scale), X)
-            np.save('../data/synthetic/y_N_{0}_M_{1}_scale_{2}'.format(N, M, scale), y)
+            X, y = make_simple_interaction_data(N, p, m0, snr)
+            np.save('../data/synthetic/X_N_{0}_M_{1}_scale_{2}'.format(N, M, snr), X)
+            np.save('../data/synthetic/y_N_{0}_M_{1}_scale_{2}'.format(N, M, snr), y)
             # Exact
             skim_model, skim_run, skim_gp, skim_time = SKIM_exact(X, y, m0, cores=2)
-            dump_pymc3_model('../model/exact_N_{0}_M_{1}_scale_{2}'.format(N, M, scale), skim_run, skim_time)
+            dump_pymc3_model('../model/exact_N_{0}_M_{1}_scale_{2}'.format(N, M, snr), skim_run, skim_time)
             for n_induce in induce_arr:
                 # Induce + Kmeans
                 print('Simulating for N = {0} and p = {1} and n_induce = {2}'.format(N, p, n_induce))
                 Xu, skim_model_induce, skim_run_induce, skim_gp_induce, skim_time_induce = SKIM_inducing(X, y, m0, 20, "FITC", False)
-                np.save('../data/synthetic/Xu_N_{0}_M_{1}_scale_{2}_induce_{3}'.format(N, M, scale, n_induce), Xu)
-                dump_pymc3_model('../model/fitc_N_{0}_M_{1}_scale_{2}_induce_{3}'.format(N, M, scale, n_induce), skim_run_induce, skim_time_induce)
+                np.save('../data/synthetic/Xu_N_{0}_M_{1}_scale_{2}_induce_{3}'.format(N, M, snr, n_induce), Xu)
+                dump_pymc3_model('../model/fitc_N_{0}_M_{1}_scale_{2}_induce_{3}'.format(N, M, snr, n_induce), skim_run_induce, skim_time_induce)
                 # Subsample
                 skim_model_sub, skim_run_sub, skim_gp_sub, skim_time_sub = SKIM_exact(X[:n_induce, :], y[:n_induce], m0)
-                dump_pymc3_model('../model/subsamp_N_{0}_M_{1}_scale_{2}_induce_{3}'.format(N, M, scale, n_induce), skim_run_sub, skim_time_sub)
+                dump_pymc3_model('../model/subsamp_N_{0}_M_{1}_scale_{2}_induce_{3}'.format(N, M, snr, n_induce), skim_run_sub, skim_time_sub)
 
